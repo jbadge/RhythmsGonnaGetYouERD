db16240940b68deb702ebd4d79c6665b6e37c853
Jeremy Badger <jeremebadger@gmail.com>
2023-04-22 13:40:48 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RhythmsGonnaGetYouERD
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# RhythmsGonnaGetYouERD
